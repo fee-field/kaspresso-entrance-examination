b7fbf5de45df5f0090b7c4dd7010532c67cbf5f3
tester <polandtiara62@gmail.com>
2025-01-22 15:19:43 +0100

    exam

--- src/main/kotlin/ru/webrelab/kie/cerealstorage/CerealStorageImpl.kt
+++ src/main/kotlin/ru/webrelab/kie/cerealstorage/CerealStorageImpl.kt
@@ -1,5 +1,7 @@
 package ru.webrelab.kie.cerealstorage
 
+import kotlin.math.min
+
 class CerealStorageImpl(
     override val containerCapacity: Float,
     override val storageCapacity: Float
@@ -20,4 +22,56 @@ class CerealStorageImpl(
 
     private val storage = mutableMapOf<Cereal, Float>()
 
+    override fun addCereal(cereal: Cereal, amount: Float): Float {
+        require(amount >= 0) {
+            "Количество крупы не может быть отрицательным"
+        }
+        checkStorageCapacity(cereal)
+        val currentAmount = storage.getOrDefault(cereal, 0f)
+        val amountForAdding = min(getSpace(cereal), amount)
+        storage[cereal] = currentAmount + amountForAdding
+        return amount - amountForAdding
+    }
+
+    override fun getCereal(cereal: Cereal, amount: Float): Float {
+        require(amount >= 0) {
+            "Количество крупы не может быть отрицательным"
+        }
+        val currentAmount = storage[cereal] ?: 0f
+        return if (currentAmount >= amount) {
+            storage[cereal] = currentAmount - amount
+            amount
+        } else {
+            storage[cereal] = 0f
+            currentAmount
+        }
+    }
+
+    override fun removeContainer(cereal: Cereal): Boolean {
+        val currentAmount = storage[cereal]
+        if (currentAmount == 0f) {
+            storage.remove(cereal)
+            return true
+        }
+        return false
+    }
+
+    override fun getAmount(cereal: Cereal): Float {
+        return storage.getOrDefault(cereal, 0f)
+    }
+
+    override fun getSpace(cereal: Cereal): Float {
+        return containerCapacity - getAmount(cereal)
+    }
+
+    override fun toString(): String {
+        return "В вашем контейнере места: $containerCapacity, а в хранилище $storageCapacity."
+    }
+
+    private fun checkStorageCapacity(cereal: Cereal) {
+        if (storage.contains(cereal)) return
+        check(storageCapacity >= (storage.size + 1) * containerCapacity) {
+            "Недостаточно места в хранилище для нового контейнера"
+        }
+    }
 }
--- src/test/kotlin/ru/webrelab/kie/cerealstorage/CerealStorageImplTest.kt
+++ src/test/kotlin/ru/webrelab/kie/cerealstorage/CerealStorageImplTest.kt
@@ -1,16 +1,166 @@
 package ru.webrelab.kie.cerealstorage
 
+import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 
 class CerealStorageImplTest {
 
-    private val storage = CerealStorageImpl(10f, 20f)
+//    private val storage = mutableMapOf(10f, 20f)
+
+    private lateinit var storage: CerealStorage
+
+    @BeforeEach
+    fun setup() {
+        storage = CerealStorageImpl(10f, 20f)
+    }
+
+// addCereal
 
     @Test
-    fun `should throw if containerCapacity is negative`() {
+    fun `добавлям крупу меньше макс объёма и проверяем, что именно такое количество добавили`() = with(storage) {
+        addCereal(Cereal.MILLET, 5.5f)
+        Assertions.assertEquals(5.5f, getAmount(Cereal.MILLET))
+    }
+
+    @Test
+    fun `добавлям крупу меньше макс объёма несколько раз и проверяем, что именно такое количество добавили`() =
+        with(storage) {
+            addCereal(Cereal.MILLET, 1.1f)
+            addCereal(Cereal.MILLET, 2.2f)
+            Assertions.assertEquals(3.3f, getAmount(Cereal.MILLET), 0.01f)
+        }
+
+    @Test
+    fun `добавлям разные крупы`() = with(storage) {
+        addCereal(Cereal.RICE, 1.1f)
+        addCereal(Cereal.PEAS, 2.2f)
+        Assertions.assertAll({ Assertions.assertEquals(1.1f, getAmount(Cereal.RICE)) },
+            { Assertions.assertEquals(2.2f, getAmount(Cereal.PEAS)) })
+    }
+
+    @Test
+    fun `добавлям больше макс объёма и проверяем, возвращает ли остаток`() {
+        val remaining = storage.addCereal(Cereal.RICE, 50f)
+        assertEquals(40f, remaining, 0.01f)
+    }
+
+    @Test
+    fun `добавлям больше макс объёма и проверяем, правильное ли количество показывает в контейнере`() {
+        val remaining = storage.addCereal(Cereal.RICE, 50f)
+        assertEquals(10f, storage.getAmount(Cereal.RICE), 0.01f)
+    }
+
+    @Test
+    fun `добавляем отрицательное количество и проверяем exception`() {
         assertThrows(IllegalArgumentException::class.java) {
-            CerealStorageImpl(-4f, 10f)
+            storage.addCereal(Cereal.RICE, -1f)
         }
     }
+
+
+// getCereal
+
+    @Test
+    fun `возвращает ли количество полученной крупы`() {
+        val cereal = Cereal.BULGUR
+        //обязательно! создаём контейнер, иначе всё падает, потому что без контейнера результат 0
+        storage.addCereal(cereal, 10f)
+        val received = storage.getCereal(cereal, 4f)
+        assertEquals(4f, received, 0.01f)
+    }
+
+    @Test
+    fun `возвращает ли остаток`() {
+        val cereal = Cereal.BULGUR
+        val received = storage.addCereal(cereal, 15f)
+        assertEquals(5f, received, 0.01f)
+    }
+
+    @Test
+    fun `вернет ли 0, если попробуем взять крупу из несуществующего контейнера`() {
+        val received = storage.getCereal(Cereal.RICE, 5f)
+        assertEquals(0f, received, 0.01f)
+    }
+
+
+// removeContainer
+
+    @Test
+    fun `удален ли контейнер, если он пустой`() {
+        val cereal = Cereal.PEAS
+        // здесь тоже сначала создаем контейнер, иначе всё валится
+        storage.addCereal(cereal, 10f)
+        storage.getCereal(cereal, 10f)
+        val result = storage.removeContainer(cereal)
+        assertTrue(result)
+    }
+
+    @Test
+    fun `НЕ удален ли контейнер, если он НЕ пустой`() {
+        val cereal = Cereal.PEAS
+        // то же
+        storage.addCereal(cereal, 9f)
+        val result = storage.removeContainer(cereal)
+        assertFalse(result)
+    }
+
+    @Test
+    fun `НЕ удален ли контейнер, если его не существует`() {
+        val result = storage.removeContainer(Cereal.RICE)
+        assertFalse(result, "Контейнера не сузествует, удаление невозможно")
+    }
+
+
+// getAmount
+
+    @Test
+    fun `cколько крупы в контейнере`() {
+        val cereal = Cereal.MILLET
+        storage.addCereal(Cereal.MILLET, 10f)
+        val inStorage = storage.getAmount(cereal)
+        assertEquals(10f, inStorage, 0.01f)
+    }
+
+    @Test
+    fun `cколько крупы в контейнере после изъятия`() {
+        val cereal = Cereal.MILLET
+        storage.addCereal(cereal, 10f)
+        storage.getCereal(cereal, 5f)
+        val inStorage = storage.getAmount(cereal)
+        assertEquals(5f, inStorage, 0.01f)
+    }
+
+    @Test
+    fun `если контейнера нет, должно вернуть 0`() {
+        assertEquals(0f, storage.getAmount(Cereal.RICE))
+    }
+
+
+    // getSpace
+    @Test
+    fun `сколько есть места изначально`() {
+        val cereal = Cereal.BUCKWHEAT
+        val storageAvailable = storage.getSpace(cereal)
+        assertEquals(10f, storageAvailable)
+    }
+
+    @Test
+    fun `сколько есть места после добавления`() {
+        val cereal = Cereal.BUCKWHEAT
+        storage.addCereal(cereal, 50f)
+        val storageAvailable = storage.getSpace(cereal)
+        assertEquals(0f, storageAvailable)
+    }
+
+//testToString
+
+    @Test
+    fun `правильно ли отображается количество `() {
+        val storage = CerealStorageImpl(10f, 20f)
+        val expected = "В вашем контейнере места: 10.0, а в хранилище 20.0."
+        assertEquals(expected, storage.toString())
+    }
 }
+
